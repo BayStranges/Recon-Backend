4cb20226e8c4c2c2657d428d199c9b8e40e11316
BayStranges <emircantari01@gmail.com>
2024-12-26 01:15:24 +0300

    mongo'da düzenlemeler yapıldı

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 
@@ -10,7 +11,32 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const User = mongoose.model('User', UserSchema);
+
+// Kayıt API'si
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+  
+  // E-posta kontrolü
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: 'Bu e-posta zaten kayıtlı!' });
+  }
+
+   // Şifreyi hashle
+   const hashedPassword = await bcrypt.hash(password, 10);
 // MongoDB bağlantısı
+// Yeni kullanıcıyı veritabanına kaydet
+const newUser = new User({
+  email,
+  password: hashedPassword
+});
+
+await newUser.save();
+
+res.status(201).json({ message: 'Kayıt başarılı!' });
+});
+
 mongoose.connect('mongodb+srv://vestia:vestia@vestia.wycebzt.mongodb.net/Vestia', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
